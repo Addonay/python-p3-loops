91b38bc2ef43a431793768d14ddf6bb2177b1a19
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-15 16:46:51 +0300

    solved the lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [n ** 2 for n in int_list]
+    return squared_list
+print(square_integers([1, 2, 3, 4, 5]))
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range (100):
+        if num % 15 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+fizzbuzz()
